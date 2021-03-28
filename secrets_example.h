23e1a12b558d58885b54b576a3cbd520ce4b97b8
Nicola Cimmino <nicola.cimmino@gmail.com>
2021-03-28 14:21:06 +0200

    Update secrets exmple

--- secrets_example.h
+++ secrets_example.h
@@ -1,9 +1,17 @@
-// Copy the content of this file into a new secrets.h file and edit with your 
+// Copy the content of this file into a new secrets.h file and edit with your
 // defisred information.
 // secrets.h is gitignored, make sure you don't commit it in case you make changes!
 
-#define PWD_TOKEN_LABEL "TST#001"
-#define PWD_TOKEN_SEAL "4C.33.FF.00"
+#ifndef __SECRETS_H__
+#define __SECRETS_H__
+
+#define PWD_TOKEN_LABEL "TST#002"
+
+#define PWD_TOKEN_SEAL_SECRET_SIZE 16
+
+uint8_t sealSecret[] = {
+    0x26, 0x25, 0x24, 0x23, 0x26, 0x25, 0x24, 0x23, 0x26, 0x25, 0x24, 0x23, 0x26, 0x25, 0x24, 0x23};
 
 #define PWD_TOKEN_PASSWORD "MySuperSecretPassword"
 
+#endif
