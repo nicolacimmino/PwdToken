27a67a56cad3403566d3d053ea8f52de2edb984f
Nicola Cimmino <nicola.cimmino@gmail.com>
2021-04-06 21:28:30 +0200

    Rename

--- TokenManifestValidator/banner/banner.go
+++
@@ -1,7 +0,0 @@
-package banner
-
-type Banner struct {
-	Otp1  uint32
-	Otp2  uint32
-	Label string
-}
--- TokenManifestValidator/manifest/manifest.go
+++
@@ -1,8 +0,0 @@
-package manifest
-
-type Manifest struct {
-	Label    string
-	Counter1 uint32
-	Counter2 uint32
-	Secret   []byte
-}
