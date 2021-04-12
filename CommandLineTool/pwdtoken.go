0608766c381f44a327da6b53a9002a18544d2c01
nicolacimmino <nicola.cimmino@gmail.com>
2021-04-12 17:45:38 +0200

    Clean

--- CommandLineTool/pwdtoken.go
+++ CommandLineTool/pwdtoken.go
@@ -1,3 +1,25 @@
+/****************************************************************************************************************************************************************
+ *  PwdToken.
+ *
+ *  Command line utility for the pwdToken.
+ *
+ *
+ *  Copyright (C) 2021 Nicola Cimmino
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http:*www.gnu.org/licenses/.
+ */
+
 package main
 
 import (
@@ -10,8 +32,8 @@ import (
 func printUsage() {
 	fmt.Println("Usage:")
 	fmt.Printf("%s [command]\n", filepath.Base(os.Args[0]))
-	fmt.Println("  ver verify a password pad banner against its manifest.")
-	fmt.Println("  gen generate a password pad manifest and secrets header.")
+	fmt.Println("  ver verify a token banner against its manifest.")
+	fmt.Println("  gen generate a token manifest and secrets header.")
 }
 
 func main() {
@@ -36,3 +58,6 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+/*
+ ****************************************************************************************************************************************************************/
